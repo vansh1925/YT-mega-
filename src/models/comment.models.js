83dde34252655b7846fe8ffb3f2822c10bfed970
viking <vworkp625@gmail.com>
2025-05-19 19:38:36 +0000

    Adding fields of tweet comment plsylist like  models

--- src/models/comment.models.js
+++ src/models/comment.models.js
@@ -1,8 +1,24 @@
 import mongoose, { Schema } from "mongoose"
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
-const commentSchema = mongoose.Schema([
+const commentSchema = mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    video: {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+
+    },
 
 
-])
+}, {
+    timestamps: true
+})
 commentSchema.plugin(mongooseAggregatePaginate)
 export const Comment = mongoose.model("Comment", commentSchema)
